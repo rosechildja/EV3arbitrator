07cbdda91a74d2fba870109cdc21280889867f72
Jesse Rosechild <rosechildja@beloit.edu>
2015-02-22 22:37:50 -0600

    Comment update + removed unnecessary line

--- src/assignment1/LightSensor.java
+++ src/assignment1/LightSensor.java
@@ -12,25 +12,24 @@ import lejos.robotics.subsumption.Behavior;
 
 public class LightSensor implements Behavior {
 	private static EV3ColorSensor cs = new EV3ColorSensor(SensorPort.S3);		//initialize color sensor cs
-	//cs.setFloodlight(false);													// turn off ambient LED
-	SensorMode brightness = cs.getAmbientMode();								// set color sensor to assess brightness
+	SensorMode brightness = cs.getAmbientMode();					// set color sensor to assess brightness
 	private boolean suppressed = false;
 		
 			public boolean takeControl() {
-				float[] csample = new float[brightness.sampleSize()];			//get color sensor state csample
-				cs.fetchSample(csample, 0);										//check array - Index 0 is 0.0 darkest, 1.0 brightest
-				while (csample[0]>.3){											//set suppressed = true if bright light
+				float[] csample = new float[brightness.sampleSize()];	//get color sensor state csample
+				cs.fetchSample(csample, 0);				//check array - Index 0 is 0.0 darkest, 1.0 brightest
+				while (csample[0]>.3){					//set suppressed = true if bright light
 					return true;												
 				}
 				return suppressed;
 			}
 
 		   public void suppress() {
-			   	suppressed = true;												//standard practice for suppress methods
+			   	suppressed = true;					//standard practice for suppress methods
 		   }
 
 		   public void action() {
-			   																	//drive backward if see bright light
+			   								//drive backward if see bright light
 			   suppressed = false;
 			   Motor.B.setSpeed(300);
 			   Motor.C.setSpeed(300);
