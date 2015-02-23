6538f071dcd4293151a632bd7046e28720a8c1d5
Jesse Rosechild <rosechildja@beloit.edu>
2015-02-22 22:32:16 -0600

    Comment update

--- src/assignment1/Arbitrator.java
+++ src/assignment1/Arbitrator.java
@@ -10,10 +10,10 @@ import lejos.robotics.subsumption.Behavior;
 public class Assignment1 {
 	
 	   public static void main(String [] args) {
-		  Behavior b1 = new DefaultState();								//4th priority behavior: drive forward
-	      Behavior b2 = new DistanceSensor();							//3rd priority behavior: check distance sensor, slow & turn response
-	      Behavior b3 = new LightSensor();								//2nd priority behavior: check light sensor, drive backward response
-	      Behavior b4 = new TouchSensor();								//1st priority behavior: check touch sensor, drive forward response
+		  Behavior b1 = new DefaultState();		//4th priority behavior: drive forward
+	      Behavior b2 = new DistanceSensor();		//3rd priority behavior: check distance sensor, slow & turn response
+	      Behavior b3 = new LightSensor();			//2nd priority behavior: check light sensor, drive backward response
+	      Behavior b4 = new TouchSensor();			//1st priority behavior: check touch sensor, drive forward response
 	      Behavior [] bArray = {b1, b2, b3, b4};
 	      Arbitrator arby = new Arbitrator(bArray);
 	      arby.start();
