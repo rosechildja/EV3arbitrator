40e226acad3dfe0d24545f4abcdad99bac661bff
Jesse Rosechild <rosechildja@beloit.edu>
2015-02-22 22:35:27 -0600

    Comment update

--- src/assignment1/DistanceSensor.java
+++ src/assignment1/DistanceSensor.java
@@ -13,33 +13,33 @@ import lejos.utility.Delay;
 
 public class DistanceSensor implements Behavior {
 	private static EV3UltrasonicSensor us = new EV3UltrasonicSensor(SensorPort.S2);	//initialize ultrasonic sensor us
-	SampleProvider distance = us.getDistanceMode();									//set ultrasonic sensor to assess distance
+	SampleProvider distance = us.getDistanceMode();					//set ultrasonic sensor to assess distance
 	private boolean suppressed = false;
 		
 			public boolean takeControl() {
 				
-				float[] usample = new float[distance.sampleSize()];					//get ultrasonic sensor state usample
-				us.fetchSample(usample, 0);											//check array - distance in meters
-				while (usample[0]<.5){												//set suppressed = true if distance < .5 meter
+				float[] usample = new float[distance.sampleSize()];	//get ultrasonic sensor state usample
+				us.fetchSample(usample, 0);				//check array - distance in meters
+				while (usample[0]<.5){					//set suppressed = true if distance < .5 meter
 					return true;												
 				}
 				return suppressed;
 			}
 
 		   public void suppress() {
-			   	suppressed = true;													//standard practice for suppress methods
+			   	suppressed = true;					//standard practice for suppress methods
 		   }
 
 		   public void action() {
-			   																		//slow if dist < .5 meters, check sensor, turn if dist < .3 meters
+			   								//slow if dist < .5 meters, check sensor, turn if dist < .3 meters
 			   suppressed = false;
 			   Motor.B.setSpeed(150);
 			   Motor.C.setSpeed(150);
 			   Motor.B.forward();
 			   Motor.C.forward();
-			   SampleProvider distance = us.getDistanceMode();						//set ultrasonic sensor to assess distance
-			   float[] usample = new float[distance.sampleSize()];					//get ultrasonic sensor state usample
-			   us.fetchSample(usample, 0);											//check array - distance in meters						
+			   SampleProvider distance = us.getDistanceMode();		//set ultrasonic sensor to assess distance
+			   float[] usample = new float[distance.sampleSize()];		//get ultrasonic sensor state usample
+			   us.fetchSample(usample, 0);					//check array - distance in meters						
 			   while (usample [0] < .3) {
 				   Motor.B.stop();
 				   Motor.C.stop();
@@ -47,8 +47,8 @@ public class DistanceSensor implements Behavior {
 				   Motor.C.setSpeed(400);
 				   Motor.B.forward();
 			       Motor.C.backward();
-			       Delay.msDelay(1100);												//wait for 1.1 seconds (prevents interrupt during turn)
-			       us.fetchSample(usample, 0);										//check array - distance in meters
+			       Delay.msDelay(1100);					//wait for 1.1 seconds (prevents interrupt during turn)
+			       us.fetchSample(usample, 0);				//check array - distance in meters
 			   }
 		   }
 }
